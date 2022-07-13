e9b999d93f168e6c4988b84991887b60d9eebbc7
Shushi <zhaobenben007@googlemail.com>
2022-07-14 00:17:50 +0200

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,9 +1,10 @@
+from jsonargparse.typing import path_type
 from abc import ABC
 
 
 class Dataset(ABC):
-    def __init(self, data_dir: str, batch_size: int = 128, augment: bool = True):
-        """Generate a dataset from image files in a directory.
+    def __init__(self, data_dir: path_type("dr"), batch_size: int = 128, augment: bool = True):
+        """Generates a dataset from image files in a directory.
         Args:
             data_dir: Directory where the images are stored
             batch_size: Number of images in a single batch
@@ -14,17 +15,17 @@ class Dataset(ABC):
         self.augment = augment
 
     def __repr__(self):
-        kv_list = [f"{key}={value}" for key, value in vars(self).items()]
-        return f"{self.__class__.__name__}: {', '.join(kv_list)}"
+        kv = [f"{key}={value}" for key, value in vars(self).items()]
+        return f"{self.__class__.__name__}: {', '.join(kv)}"
 
 
 class Cifar10Dataset(Dataset):
     def __init__(self, *args, **kwargs):
-        """Generate a dataset from CIFAR10 image files in a directory."""
+        """Generates a dataset from CIFAR10 image files in a directory."""
         super().__init__(*args, **kwargs)
 
 
 class ImageNetDataset(Dataset):
     def __init__(self, *args, **kwargs):
-        """Generate a dataset from ImageNet image files in a directory."""
+        """Generates a dataset from ImageNet image files in a directory."""
         super().__init__(*args, **kwargs)
