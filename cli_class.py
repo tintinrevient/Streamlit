cc36a500d4ce4c976ba05ca12bb51c2394c45d57
Shushi <zhaobenben007@googlemail.com>
2022-07-14 00:17:23 +0200

    Update cli_class.py

--- cli_class.py
+++ cli_class.py
@@ -4,9 +4,15 @@ from model import Model
 
 
 def train(dataset: Dataset, model: Model):
+    """Trains a model on the given dataset.
+
+    Args:
+        dataset: Image dataset
+        model: Neural network
+    """
     print(f"Train using:")
-    print(f"   {dataset}")
-    print(f"   {model}")
+    print(f"  {dataset}")
+    print(f"  {model}")
 
 
 if __name__ == '__main__':
@@ -15,8 +21,8 @@ if __name__ == '__main__':
     # python cli_class.py --help
 
     # python cli_class.py --dataset ImageNetDataset --model ConvNet --print_config
-    # python cli_class.py --dataset ImageNetDataset --model ConvNet --print_config > config/dataset_model.yaml
+    # python cli_class.py --dataset ImageNetDataset --model ConvNet --print_config > config/all.yaml
 
-    # python cli_class.py --config config/dataset_model.yaml
+    # python cli_class.py --config config/all.yaml
     # python cli_class.py --dataset config/dataset.yaml --model config/model.yaml
-    # python cli_class.py --config config/sub_config.yaml
+    # python cli_class.py --config config/all_subconfig.yaml
